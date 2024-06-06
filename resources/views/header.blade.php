4b437b40f0f59a3759704d677c6de7eb5c3ae6e5
Mhtal123 <mht.alhafiz2002@gmail.com>
2024-06-06 07:41:01 +0100

    Modification de la page d'accueil - Ajout d'une couleur unique par logo de bureau - Logo du geometre modifie - Image de fond remplacee - Gradiant propre a chaque bureau - Background du header mis en noir

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -12,12 +12,12 @@
 </head>
 
 <header>
-<nav class="navbar navbar-expand-sm">
+<nav class="navbar bg-dark navbar-expand-sm">
   <div class="container-fluid row d-flex p=0 m-0">
     <a class="navbar-brand col-3 border-end border-grey border-2 text-decoration-none" href="/" id="brand">Edje'el</a>
     @auth
     <div class="container-fluid col-7 d-flex justify-content-start">
-        <span class="ms-0">Bienvenue, {{Auth::user()->name}}</span>
+        <span class="ms-0 text-white">Bienvenue, {{Auth::user()->name}}</span>
     </div>
     <div class="container-fluid col-2">
         <form class="d-flex justify-content-end" action="{{route('auth.logout')}}" method="POST">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <!-- First Row -->
     <div class="container d-flex justify-content-center" id="first-row">
         <!-- Bureau du chef -->
-        <a class="container m-0 p-0 rectangle2" href="{{route('user.chef')}}">
+        <a class="container m-0 p-0 rectangle2" id="chef" href="{{route('user.chef')}}">
             <div class="container-fluid justify-content-center d-flex flex-wrap align-content-center icons8_off">
                 <img src="{{asset('img/OfficeChairIcon.png')}}">
             </div>
@@ -13,17 +13,17 @@
         </a>
 
         <!-- BAG -->
-        <a class="container m-0 ms-5 p-0 rectangle2" href="{{route('user.bag')}}">
+        <a class="container m-0 ms-5 p-0 rectangle2" id="bag" href="{{route('user.bag')}}">
             <div class="container-fluid justify-content-center d-flex flex-wrap align-content-center icons8_off">
-                <img class="" src="{{asset('img/PrinterIcon.png')}}">
+                <img src="{{asset('img/Printer.png')}}">
             </div>
             <p class="bureau">Bureau des Affaires Generales</p>
         </a>
 
         <!-- BMJ -->
-        <a class="container m-0 ms-5 p-0 rectangle2" href="{{route('user.bmj')}}">
+        <a class="container m-0 ms-5 p-0 rectangle2" id="bmj" href="{{route('user.bmj')}}">
             <div class="container-fluid justify-content-center d-flex flex-wrap align-content-center icons8_off">
-                <img src="{{asset('img/UpdateLeftRotationIcon.png')}}">
+                <img src="{{asset('img/Update.png')}}">
             </div>
             <p class="bureau">Bureau des Mises a Jour</p>
         </a>
@@ -32,24 +32,23 @@
     <!-- Second Row -->
     <div class="container d-flex justify-content-center" id="second-row">
         <!-- Bureau du controle -->
-
-        <a class="container m-0 p-0 rectangle2" href="{{route('user.bc')}}">
+        <a class="container m-0 p-0 rectangle2" id="bc" href="{{route('user.bc')}}">
             <div class="container-fluid justify-content-center d-flex flex-wrap align-content-center icons8_off">
-                <img class="" src="{{asset('img/ProtectIcon.png')}}">
+                <img class="" src="{{asset('img/ProtectIcon2.png')}}">
             </div>
             <p class="bureau">Bureau de Controle</p>
         </a>
 
         <!-- Geometre -->
-        <a class="container m-0 ms-5 p-0 rectangle2" href="{{route('user.geometre')}}">
+        <a class="container m-0 ms-5 p-0 rectangle2" id="geometre" href="{{route('user.geometre')}}">
             <div class="container-fluid justify-content-center d-flex flex-wrap align-content-center icons8_off">
-                <img class="" src="{{asset('img/DrawingCompassIcon.png')}}">
+                <img class="" src="{{asset('img/tripod.png')}}">
             </div>
             <p class="bureau">Geometre</p>
         </a>
 
         <!-- Archivages -->
-        <a class="container m-0 ms-5 p-0 rectangle2" href="{{route('user.archivage')}}">
+        <a class="container m-0 ms-5 p-0 rectangle2" id="archive" href="{{route('user.archivage')}}">
             <div class="container-fluid justify-content-center d-flex flex-wrap align-content-center icons8_off">
                 <img class="" src="{{asset('img/WinrarIcon.png')}}">
             </div>
