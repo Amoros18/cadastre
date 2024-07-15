1b63f89a8a1d67b97b0dde8be4b39950754b9a36
Mhtal123 <mht.alhafiz2002@gmail.com>
2024-07-15 09:57:53 +0100

    Correction du titre de l'onglet

--- resources/views/bc/accueil.blade.php
+++ resources/views/bc/accueil.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>@yeild('title')</title>
+    <title>@yield('title')</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
